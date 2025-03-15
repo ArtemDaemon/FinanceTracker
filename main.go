2cbc7aee75132638babafec7565ed45b70bd65ed
ArtemDaemon <artem.daemon.official@gmail.com>
2025-03-15 13:19:05 +0700

    Добавил навигационное меню

--- main.go
+++ main.go
@@ -7,6 +7,30 @@ import (
 	"text/template"
 )
 
+func renderTemplate(w http.ResponseWriter, tmpl string, data map[string]interface{}) {
+	tmpls, err := template.ParseFiles("html/layout.html", "html/"+tmpl+".html")
+	if err != nil {
+		http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+		return
+	}
+
+	tmpls.Execute(w, data)
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{
+		"Title": "Главная",
+	}
+	renderTemplate(w, "main", data)
+}
+
+func transactionsPage(w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{
+		"Title": "Транзакции",
+	}
+	renderTemplate(w, "transactions", data)
+}
+
 func main() {
 	database.InitDB()
 	defer database.DB.Close()
@@ -15,11 +39,6 @@ func main() {
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("html/img"))))
 
 	http.HandleFunc("/main/", mainPage)
+	http.HandleFunc("/transactions/", transactionsPage)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func mainPage(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("html/nav_menu.html")
-
-	t.Execute(w, nil)
 }
