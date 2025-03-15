c00158b10535606d0cf085f5b4c40d63d85a2fb5
ArtemDaemon <artem.daemon.official@gmail.com>
2025-03-15 19:01:45 +0700

    Добавил форматированный вывод данных текущего баланса

--- database/sql/get_total_balance.sql
+++ database/sql/get_total_balance.sql
@@ -1,4 +1,8 @@
 SELECT
+	CAST((SELECT value FROM config WHERE key = 'start_balance') AS NUMERIC) as start_balance,
+	COALESCE(SUM(CASE WHEN transaction_type = 2 THEN amount ELSE 0 END), 0) AS total_income,
+    COALESCE(SUM(CASE WHEN transaction_type = 1 THEN -amount ELSE 0 END), 0) AS total_expense,
+    COALESCE(SUM(CASE WHEN transaction_type = 2 THEN amount ELSE -amount END), 0) AS balance_delta,
     CAST((SELECT value FROM config WHERE key = 'start_balance') AS NUMERIC)
     +
     COALESCE(SUM(CASE WHEN transaction_type = 2 THEN amount ELSE -amount END), 0) AS current_balance
