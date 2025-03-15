12b9e0557be0345cf592d8c95504cb3d00c74af9
ArtemDaemon <artem.daemon.official@gmail.com>
2025-03-15 17:46:14 +0700

    Добавил инициализацию БД и структуры данных

--- database/sql/get_total_balance.sql
+++ database/sql/get_total_balance.sql
@@ -1,6 +1,6 @@
 SELECT
-	CAST((SELECT value FROM config WHERE key = 'start_balance') AS NUMERIC)
-	+
-	SUM(CASE WHEN transaction_type = 2 THEN amount ELSE -amount END) AS current_balance
+    CAST((SELECT value FROM config WHERE key = 'start_balance') AS NUMERIC)
+    +
+    COALESCE(SUM(CASE WHEN transaction_type = 2 THEN amount ELSE -amount END), 0) AS current_balance
 FROM
-	transactions
+    transactions;
---
+++ database/sql/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS categories (
+	category_id	INTEGER NOT NULL UNIQUE,
+	category_name	TEXT NOT NULL,
+	PRIMARY KEY(category_id AUTOINCREMENT)
+);
+
+INSERT OR IGNORE INTO categories (category_name) 
+VALUES ('Продукты');
+
+CREATE TABLE IF NOT EXISTS config (
+	key	TEXT NOT NULL UNIQUE,
+	value	TEXT,
+	PRIMARY KEY(key)
+);
+
+INSERT OR IGNORE INTO config (key, value) VALUES ('start_balance', '0');
+
+CREATE TABLE transactions (
+	transaction_id	INTEGER NOT NULL UNIQUE,
+	category_id	INTEGER NOT NULL,
+	amount	REAL NOT NULL DEFAULT 0,
+	transaction_type	INTEGER NOT NULL DEFAULT 1,
+	"ate"TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+	PRIMARY KEY(transaction_id AUTOINCREMENT),
+	CONSTRAINT category_fk FOREIGN KEY(category_id) REFERENCES categories(category_id)
+);
